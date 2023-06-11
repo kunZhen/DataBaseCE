d62032088967485d9a0f58d4acbe9a0a4944e996
Kun Kin Zheng Liang <106352914+kunZhen@users.noreply.github.com>
2023-06-11 02:05:24 -0600

    Cartas para cada operación

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     ChrisListComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- FrontEnd/src/app/components/home/home.component.ts
+++ FrontEnd/src/app/components/home/home.component.ts
@@ -6,7 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  // CARD STATUS
+  createCardStatus: boolean = false;
+  selectCardStatus: boolean = false;
+  insertCardStatus: boolean = false;
+  deleteCardStatus: boolean = false;
+  updateCardStatus: boolean = false;
+
+
   ngOnInit(): void {
 
   }
+
+  //submit
+  submit() {
+    console.log("submitted ");
+  }
+
+
+  // Change card status
+  createCardChange() {
+    this.createCardStatus = !this.createCardStatus;
+    console.log("createCardStatus: ", this.createCardStatus);
+  }
+
+  selectCardChange() {
+    this.selectCardStatus = !this.selectCardStatus;
+    console.log("selectCardStatus: ", this.selectCardStatus);
+  }
+
+  insertCardChange() {
+    this.insertCardStatus = !this.insertCardStatus;
+    console.log("insertCardSatatus: ", this.insertCardStatus);
+  }
+
+  deleteCardChange() {
+    this.deleteCardStatus = !this.deleteCardStatus;
+    console.log("deleteCardStatus: ", this.deleteCardStatus);
+  }
+
+  updateCardChange() {
+    this.updateCardStatus = !this.updateCardStatus;
+    console.log("updateCardStatus: ", this.updateCardStatus);
+  }
+
+
+
 }
